7147177af1fa204059d073bde8f60e69c35e6b60
BrandonAbe <28733924+BrandonAbe@users.noreply.github.com>
2025-06-01 23:45:51 -0400

    Refactor to allow change between celcius and fahrenheit

--- app.py
+++ app.py
@@ -11,21 +11,32 @@ API_KEY = os.getenv('OPENWEATHER_API_KEY')
 @app.route('/', methods=['GET', 'POST'])
 def index():
     weather_data = None
+    city = ''
+    unit = 'metric'
+    unit_symbol = '°C'
+
     if request.method == 'POST':
-        city = request.form['city']
-        url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric'
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            weather_data = {
-                'city': city,
-                'temperature': data['main']['temp'],
-                'description': data['weather'][0]['description'],
-                'icon': data['weather'][0]['icon']
-            }
-        else:
-            weather_data = {'error': 'City not found'}
-    return render_template('index.html', weather=weather_data)
+        city = request.form.get('city', '')
+        unit = request.form.get('unit', 'metric')
+        unit_symbol = '°C' if unit == 'metric' else '°F'
+
+        if city:
+            url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units={unit}'
+            response = requests.get(url)
+            if response.status_code == 200:
+                data = response.json()
+                weather_data = {
+                    'city': city,
+                    'temperature': data['main']['temp'],
+                    'description': data['weather'][0]['description'],
+                    'icon': data['weather'][0]['icon'],
+                    'unit': unit_symbol
+                }
+            else:
+                weather_data = {'error': 'City not found'}
+
+    return render_template('index.html', weather=weather_data, city=city, unit=unit)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
